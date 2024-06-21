3d68b8ff88346f2bb9e87f2f7a7dd4ee67a4b7f6
AliyevaTukazban <tukazbanaliyeva@yahoo.com>
2024-06-22 00:19:38 +0200

    1.Programmieraufgabe.md - Screenshots erstellt. 2.Api gefixt 3.Lighthouse- alles grün 22.06.2024

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import './style.css';
+import './Style.css';
 import App from './App.vue';
 import MensaList from './components/MensaList.vue';
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,19 @@
-import { defineConfig } from 'vite'; // Importieren der defineConfig Funktion von Vite
-import vue from '@vitejs/plugin-vue'; // Importieren des Vue Plugins für Vite
-import { VitePWA } from 'vite-plugin-pwa'; // Importieren des Vite PWA Plugins
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [vue(), // Verwenden des Vue Plugins
+  plugins: [
+    vue(),
     VitePWA({
-      registerType: 'autoUpdate', // Automatisches Aktualisieren des Service Workers
-      strategies: 'injectManifest', // Verwenden der injectManifest Strategie für PWA, um einen eigenen Service Worker zu implementieren
-      srcDir: 'src', // Quellverzeichnis für den Service Worker
-      filename: 'my-service-worker.ts', // Dateiname des Service Workers
-      injectManifest: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        maximumFileSizeToCacheInBytes: 3000000
-      },
+      registerType: 'autoUpdate',
       manifest: {
-        name: 'Vite + Vue PWA Starter by Robin', // Der Name deiner PWA
-        short_name: 'ViteVuePWAStarter', // // Kurzer Name deiner PWA
-        theme_color: '#ffffff', // Designfarbe Ihrer PWA
+        name: 'Mensa Marvel App',
+        short_name: 'Mensa Marvel',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        start_url: '/',
+        display: 'standalone',
         icons: [
           {
             src: 'icons/icon-192x192.png',
@@ -24,31 +21,11 @@ export default defineConfig({
             type: 'image/png',
           },
           {
-            src: 'icons/icon-256x256.png',
-            sizes: '256x256',
-            type: 'image/png',
-          },
-          {
-            src: 'icons/icon-384x384.png',
-            sizes: '384x384',
-            type: 'image/png',
-          },
-          {
             src: 'icons/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-          },
-          {
-            src: 'icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
           },
         ],
-      },
-      devOptions: {
-        enabled: true, // Aktivieren des Service Workers während der Entwicklung
-        type: 'module', // Modultyp für den Service Worker
       },
     }),
   ],
