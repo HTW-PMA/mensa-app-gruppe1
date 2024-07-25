4418a56a1273a11e10666ce8a3dbe1c7b6522aca
David Seese <David-Seese@outlook.com>
2024-07-25 13:59:35 +0200

    added temp data

---
+++ src/types/tmpDataMensa.ts
@@ -0,0 +1,560 @@
+export const CANTEEN_DEBUG_DATA = [
+    {
+        "name": "Mensa ASH Berlin Hellersdorf",
+        "address": {
+            "street": "Alice-Salomon-Platz 5",
+            "city": "Berlin",
+            "zipcode": "12627",
+            "district": "Marzahn-Hellersdorf",
+            "geoLocation": {
+                "longitude": 13.60608822398341,
+                "latitude": 52.53719905
+            }
+        },
+        "contactInfo": {
+            "email": "mensen@stw.berlin",
+            "phone": "+4930939397721"
+        },
+        "url": "mensen/einrichtungen/mensa-alice-salomon-hs-hellersdorf.html",
+        "clickAndCollect": false,
+        "businessDays": [
+            {
+                "day": "Mo",
+                "businessHours": [
+                    {
+                        "openAt": "08:00",
+                        "closeAt": "15:00",
+                        "businessHourType": "Mensa"
+                    },
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Di",
+                "businessHours": [
+                    {
+                        "openAt": "08:00",
+                        "closeAt": "15:00",
+                        "businessHourType": "Mensa"
+                    },
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Mi",
+                "businessHours": [
+                    {
+                        "openAt": "08:00",
+                        "closeAt": "15:00",
+                        "businessHourType": "Mensa"
+                    },
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Do",
+                "businessHours": [
+                    {
+                        "openAt": "08:00",
+                        "closeAt": "15:00",
+                        "businessHourType": "Mensa"
+                    },
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Fr",
+                "businessHours": [
+                    {
+                        "openAt": "08:00",
+                        "closeAt": "15:00",
+                        "businessHourType": "Mensa"
+                    },
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Sa",
+                "businessHours": []
+            },
+            {
+                "day": "So",
+                "businessHours": []
+            }
+        ],
+        "canteenReviewData": [],
+        "universities": [
+            "Alice Salomon Hochschule Berlin"
+        ],
+        "lastUpdated": "2024-07-18T00:01:56.277Z",
+        "id": "655ff175136d3b580c970f7b"
+    },
+    {
+        "name": "Mensa HfM Charlottenstraße",
+        "address": {
+            "street": "Charlottenstraße 55",
+            "city": "Berlin",
+            "zipcode": "10117",
+            "district": "Mitte",
+            "geoLocation": {
+                "longitude": 13.3908638,
+                "latitude": 52.5135064
+            }
+        },
+        "contactInfo": {
+            "email": "mensen@stw.berlin",
+            "phone": "+49030939397631"
+        },
+        "url": "mensen/einrichtungen/hfm/mensa-hfm-charlottenstraße.html",
+        "clickAndCollect": false,
+        "businessDays": [
+            {
+                "day": "Mo",
+                "businessHours": [
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Di",
+                "businessHours": [
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Mi",
+                "businessHours": [
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Do",
+                "businessHours": [
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Fr",
+                "businessHours": [
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Sa",
+                "businessHours": []
+            },
+            {
+                "day": "So",
+                "businessHours": []
+            }
+        ],
+        "canteenReviewData": [],
+        "universities": [
+            "Barenboim-Said Akademie",
+            "Deutsche Film- und Fernsehakademie Berlin",
+            "Hertie School",
+            "Hochschule für Musik Hanns Eisler Berlin"
+        ],
+        "lastUpdated": "2024-07-18T00:01:56.34Z",
+        "id": "655ff175136d3b580c970f7c"
+    },
+    {
+        "name": "Mensa BHT Luxemburger Straße",
+        "address": {
+            "street": "Luxemburger Straße 9",
+            "city": "Berlin",
+            "zipcode": "13353",
+            "district": "Mitte",
+            "geoLocation": {
+                "longitude": 13.3542215,
+                "latitude": 52.545255
+            }
+        },
+        "contactInfo": {
+            "email": "mensen@stw.berlin",
+            "phone": "+49030939397481"
+        },
+        "url": "mensen/einrichtungen/berliner-hochschule-für-technik/mensa-bht.html",
+        "clickAndCollect": false,
+        "businessDays": [
+            {
+                "day": "Mo",
+                "businessHours": [
+                    {
+                        "openAt": "08:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mensa"
+                    },
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Di",
+                "businessHours": [
+                    {
+                        "openAt": "08:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mensa"
+                    },
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Mi",
+                "businessHours": [
+                    {
+                        "openAt": "08:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mensa"
+                    },
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Do",
+                "businessHours": [
+                    {
+                        "openAt": "08:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mensa"
+                    },
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Fr",
+                "businessHours": [
+                    {
+                        "openAt": "08:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mensa"
+                    },
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Sa",
+                "businessHours": []
+            },
+            {
+                "day": "So",
+                "businessHours": []
+            }
+        ],
+        "canteenReviewData": [],
+        "universities": [
+            "Berliner Hochschule für Technik "
+        ],
+        "lastUpdated": "2024-07-18T00:01:56.347Z",
+        "id": "655ff175136d3b580c970f7d"
+    },
+    {
+        "name": "Mensa-Backshop BHT Luxemburger Straße",
+        "address": {
+            "street": "Luxemburger Straße 9",
+            "city": "Berlin",
+            "zipcode": "13353",
+            "district": "Mitte",
+            "geoLocation": {
+                "longitude": 13.3542215,
+                "latitude": 52.545255
+            }
+        },
+        "contactInfo": {
+            "email": "mensen@stw.berlin",
+            "phone": "+49030939397481"
+        },
+        "url": "mensen/einrichtungen/berliner-hochschule-für-technik/backshop-bht-luxemburger-straße.html",
+        "clickAndCollect": false,
+        "businessDays": [
+            {
+                "day": "Mo",
+                "businessHours": [
+                    {
+                        "openAt": "10:00",
+                        "closeAt": "16:00",
+                        "businessHourType": "Mensa"
+                    }
+                ]
+            },
+            {
+                "day": "Di",
+                "businessHours": [
+                    {
+                        "openAt": "10:00",
+                        "closeAt": "16:00",
+                        "businessHourType": "Mensa"
+                    }
+                ]
+            },
+            {
+                "day": "Mi",
+                "businessHours": [
+                    {
+                        "openAt": "10:00",
+                        "closeAt": "16:00",
+                        "businessHourType": "Mensa"
+                    }
+                ]
+            },
+            {
+                "day": "Do",
+                "businessHours": [
+                    {
+                        "openAt": "10:00",
+                        "closeAt": "16:00",
+                        "businessHourType": "Mensa"
+                    }
+                ]
+            },
+            {
+                "day": "Fr",
+                "businessHours": [
+                    {
+                        "openAt": "10:00",
+                        "closeAt": "16:00",
+                        "businessHourType": "Mensa"
+                    }
+                ]
+            },
+            {
+                "day": "Sa",
+                "businessHours": []
+            },
+            {
+                "day": "So",
+                "businessHours": []
+            }
+        ],
+        "canteenReviewData": [],
+        "universities": [
+            "Berliner Hochschule für Technik "
+        ],
+        "lastUpdated": "2024-07-18T00:01:56.448Z",
+        "id": "655ff175136d3b580c970f7e"
+    },
+    {
+        "name": "Mensa-Späti BHT Haus Grashof",
+        "address": {
+            "street": "Luxemburger Straße 10",
+            "city": "Berlin",
+            "zipcode": "13353",
+            "district": "Mitte",
+            "geoLocation": {
+                "longitude": 13.353284,
+                "latitude": 52.5444812
+            }
+        },
+        "contactInfo": {
+            "email": "mensen@stw.berlin",
+            "phone": "+49030939397488"
+        },
+        "url": "mensen/einrichtungen/berliner-hochschule-für-technik/späti-bht-haus-grashof.html",
+        "clickAndCollect": false,
+        "businessDays": [
+            {
+                "day": "Mo",
+                "businessHours": []
+            },
+            {
+                "day": "Di",
+                "businessHours": []
+            },
+            {
+                "day": "Mi",
+                "businessHours": []
+            },
+            {
+                "day": "Do",
+                "businessHours": []
+            },
+            {
+                "day": "Fr",
+                "businessHours": []
+            },
+            {
+                "day": "Sa",
+                "businessHours": []
+            },
+            {
+                "day": "So",
+                "businessHours": []
+            }
+        ],
+        "canteenReviewData": [],
+        "universities": [
+            "Berliner Hochschule für Technik "
+        ],
+        "lastUpdated": "2024-07-18T00:01:56.473Z",
+        "id": "655ff175136d3b580c970f7f"
+    },
+    {
+        "name": "Mensa HTW Treskowallee",
+        "address": {
+            "street": "Treskowallee 8",
+            "city": "Berlin",
+            "zipcode": "10318",
+            "district": "Lichtenberg",
+            "geoLocation": {
+                "longitude": 13.52462014222825,
+                "latitude": 52.4931964
+            }
+        },
+        "contactInfo": {
+            "email": "mensen@stw.berlin",
+            "phone": "+49030939397550"
+        },
+        "url": "mensen/einrichtungen/hochschule-für-technik-und-wirtschaft-berlin/mensa-htw-treskowallee.html",
+        "clickAndCollect": false,
+        "businessDays": [
+            {
+                "day": "Mo",
+                "businessHours": [
+                    {
+                        "openAt": "08:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mensa"
+                    },
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Di",
+                "businessHours": [
+                    {
+                        "openAt": "08:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mensa"
+                    },
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Mi",
+                "businessHours": [
+                    {
+                        "openAt": "08:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mensa"
+                    },
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Do",
+                "businessHours": [
+                    {
+                        "openAt": "08:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mensa"
+                    },
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Fr",
+                "businessHours": [
+                    {
+                        "openAt": "08:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mensa"
+                    },
+                    {
+                        "openAt": "11:00",
+                        "closeAt": "14:30",
+                        "businessHourType": "Mittagstisch"
+                    }
+                ]
+            },
+            {
+                "day": "Sa",
+                "businessHours": []
+            },
+            {
+                "day": "So",
+                "businessHours": []
+            }
+        ],
+        "canteenReviewData": [],
+        "universities": [
+            "Hochschule für Technik und Wirtschaft Berlin"
+        ],
+        "lastUpdated": "2024-07-18T00:01:56.75Z",
+        "id": "655ff175136d3b580c970f80"
+    },
+]
